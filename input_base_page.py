caeef10ca31e8eb0a1cb12d4820625a80e7cce17
Dave Hunt <dave.hunt@gmail.com>
2011-01-05 23:08:26 +0000

    Added my name to the contributors list.

--- input_base_page.py
+++ input_base_page.py
@@ -23,6 +23,7 @@
 # the Initial Developer. All Rights Reserved.
 #
 # Contributor(s): Vishal
+#                 Dave Hunt <dhunt@mozilla.com>
 #
 # Alternatively, the contents of this file may be used under the terms of
 # either the GNU General Public License Version 2 or later (the "GPL"), or
--- search_results_page.py
+++ search_results_page.py
@@ -21,6 +21,7 @@
 #
 # Contributor(s): Vishal
 #                 David Burns
+#                 Dave Hunt <dhunt@mozilla.com>
 #
 # Alternatively, the contents of this file may be used under the terms of
 # either the GNU General Public License Version 2 or later (the "GPL"), or

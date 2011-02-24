9c947feeed29f068cd9d88292e5b4a9da8b1e192
Dave Hunt <dave.hunt@gmail.com>
2011-02-24 18:23:02 +0000

    Added myself to the contributor list.

--- test_locale_filter.py
+++ test_locale_filter.py
@@ -1,7 +1,4 @@
 #!/usr/bin/env python
-
-# -*- coding: utf-8 -*-
-
 # ***** BEGIN LICENSE BLOCK *****
 # Version: MPL 1.1/GPL 2.0/LGPL 2.1
 #
@@ -19,10 +16,11 @@
 #
 # The Initial Developer of the Original Code is
 # Mozilla.
-# Portions created by the Initial Developer are Copyright (C) 2010
+# Portions created by the Initial Developer are Copyright (C) 2011
 # the Initial Developer. All Rights Reserved.
 #
 # Contributor(s): Tobias Markus <tobbi.bugs@googlemail.com>
+#                 Dave Hunt <dhunt@mozilla.com>
 #
 # Alternatively, the contents of this file may be used under the terms of
 # either the GNU General Public License Version 2 or later (the "GPL"), or

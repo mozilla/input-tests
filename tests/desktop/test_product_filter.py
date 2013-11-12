506d3daeef5c2317fe7f41abbc6e23b938e68c61
Sergey Tupchiy <sergey.tupchiy@optile.de>
2013-11-12 10:14:24 +0100

    github-150 - update test_product_filter

--- tests/desktop/test_product_filter.py
+++ tests/desktop/test_product_filter.py
@@ -14,8 +14,8 @@ from pages.desktop.sites import SitesPage
 class TestProductFilter:
 
     @pytest.mark.nondestructive
-    def test_feedback_can_be_filtered_by_firefox_versions(self, mozwebqa):
-        """This testcase covers # 13602 & 13603 in Litmus.
+    def test_feedback_can_be_filtered_by_all_products_and_versions(self, mozwebqa):
+        """This testcase covers # 13602 & 13603 & 15149 in Litmus.
 
         1. Verify that at least three firefox versions exist
         2. Verify that filtering by version returns results
@@ -25,139 +25,21 @@ class TestProductFilter:
         """
         feedback_pg = FeedbackPage(mozwebqa)
 
-        product = "firefox"
         feedback_pg.go_to_feedback_page()
-        feedback_pg.product_filter.select_product(product)
-        versions = feedback_pg.product_filter.versions
-        [Assert.not_equal(version, "") for version in versions]
-        Assert.greater(len(versions), 3)
-        for version in [versions[2], versions[-1]]:
-            print "Checking %s version '%s'." % (product, version)
-            feedback_pg.product_filter.select_version(version)
-            Assert.equal(feedback_pg.product_filter.selected_product, product)
-            Assert.equal(feedback_pg.product_filter.selected_version, version)
-            Assert.equal(feedback_pg.product_from_url, product)
-            Assert.equal(feedback_pg.version_from_url, version)
-
-    @pytest.mark.xfail(reason='Bug 733787 - Default version not set correctly')
-    @pytest.mark.nondestructive
-    def test_feedback_can_be_displayed_for_all_firefox_versions(self, mozwebqa):
-        """This testcase covers # 15149 in Litmus.
-
-        1. Verify that filtering by all versions returns results
-        2. Verify that the state of the filters are correct after being applied
-        3. Verify product and version values in the URL
-
-        """
-        feedback_pg = FeedbackPage(mozwebqa)
-
-        product = "firefox"
-        version = "--"
-        feedback_pg.go_to_feedback_page()
-        feedback_pg.product_filter.select_product(product)
-        feedback_pg.product_filter.select_version(version)
-        Assert.equal(feedback_pg.product_filter.selected_product, product)
-        Assert.equal(feedback_pg.product_filter.selected_version, version)
-        Assert.equal(feedback_pg.product_from_url, product)
-        Assert.equal(feedback_pg.version_from_url, version)
-
-    @pytest.mark.nondestructive
-    def test_feedback_can_be_filtered_by_mobile_versions(self, mozwebqa):
-        """This testcase covers # 13602 & 13604 in Litmus.
-
-        1. Verify that at least three mobile versions exist
-        2. Verify that filtering by version returns results
-        3. Verify that the state of the filters are correct after being applied
-        4. Verify product and version values in the URL
-
-        """
-        feedback_pg = FeedbackPage(mozwebqa)
-
-        product = "mobile"
-        feedback_pg.go_to_feedback_page()
-        feedback_pg.product_filter.select_product(product)
-        versions = feedback_pg.product_filter.versions
-        [Assert.not_equal(version, "") for version in versions]
-        Assert.greater(len(versions), 3)
-        for version in [versions[2], versions[-1]]:
-            print "Checking %s version '%s'." % (product, version)
-            feedback_pg.product_filter.select_version(version)
-            Assert.equal(feedback_pg.product_filter.selected_product, product)
-            Assert.equal(feedback_pg.product_filter.selected_version, version)
-            Assert.equal(feedback_pg.product_from_url, product)
-            Assert.equal(feedback_pg.version_from_url, version)
-
-    @pytest.mark.nondestructive
-    def test_feedback_can_be_displayed_for_all_mobile_versions(self, mozwebqa):
-        """This testcase covers # 15377 in Litmus.
-
-        1. Verify that filtering by all versions returns results
-        2. Verify that the state of the filters are correct after being applied
-        3. Verify product and version values in the URL
-
-        """
-        feedback_pg = FeedbackPage(mozwebqa)
-
-        product = "mobile"
-        version = "--"
-        feedback_pg.go_to_feedback_page()
-        feedback_pg.product_filter.select_product(product)
-        feedback_pg.product_filter.select_version(version)
-        Assert.equal(feedback_pg.product_filter.selected_product, product)
-        Assert.equal(feedback_pg.product_filter.selected_version, version)
-        Assert.equal(feedback_pg.product_from_url, product)
-        Assert.equal(feedback_pg.version_from_url, version)
-
-    @pytest.mark.xfail(reason='Bug 733787 - Default version not set correctly')
-    @pytest.mark.nondestructive
-    def test_sites_can_be_filtered_by_firefox_versions(self, mozwebqa):
-        """This testcase covers # 15043 & 15045 in Litmus.
-
-        1. Verify that at least three firefox versions exist
-        2. Verify that filtering by version returns results
-        3. Verify that the state of the filters are correct after being applied
-        4. Verify product and version values in the URL
-
-        """
-        sites_pg = SitesPage(mozwebqa)
-
-        product = "firefox"
-        sites_pg.go_to_sites_page()
-        sites_pg.product_filter.select_product(product)
-        versions = sites_pg.product_filter.versions
-        [Assert.not_equal(version, "") for version in versions]
-        Assert.greater(len(versions), 2)
-        for version in [versions[1], versions[-1]]:
-            print "Checking %s version '%s'." % (product, version)
-            sites_pg.product_filter.select_version(version)
-            Assert.equal(sites_pg.product_filter.selected_product, product)
-            Assert.equal(sites_pg.product_filter.selected_version, version)
-            Assert.equal(sites_pg.product_from_url, product)
-            Assert.equal(sites_pg.version_from_url, version)
-
-    @pytest.mark.xfail(Reason='Bug 716854 - Unable to select mobile product from Sites page')
-    @pytest.mark.nondestructive
-    def test_sites_can_be_filtered_by_mobile_versions(self, mozwebqa):
-        """This testcase covers # 15043 & 15044 in Litmus.
-
-        1. Verify that at least three mobile versions exist
-        2. Verify that filtering by version returns results
-        3. Verify that the state of the filters are correct after being applied
-        4. Verify product and version values in the URL
-
-        """
-        sites_pg = SitesPage(mozwebqa)
-
-        product = "mobile"
-        sites_pg.go_to_sites_page()
-        sites_pg.product_filter.select_product(product)
-        versions = sites_pg.product_filter.versions
-        [Assert.not_equal(version, "") for version in versions]
-        Assert.greater(len(versions), 2)
-        for version in [versions[1], versions[-1]]:
-            print "Checking %s version '%s'." % (product, version)
-            sites_pg.product_filter.select_version(version)
-            Assert.equal(sites_pg.product_filter.selected_product, product)
-            Assert.equal(sites_pg.product_filter.selected_version, version)
-            Assert.equal(sites_pg.product_from_url, product)
-            Assert.equal(sites_pg.version_from_url, version)
+        products = feedback_pg.product_filter.products
+        Assert.greater(len(products), 1)
+        for product in products:
+            if product != '':
+                feedback_pg.product_filter.select_product(product)
+                versions = feedback_pg.product_filter.versions
+                [Assert.not_equal(version, "") for version in versions]
+                Assert.greater(len(versions), 2)
+                for version in versions:
+                    feedback_pg.product_filter.select_version(version)
+                    Assert.equal(feedback_pg.product_filter.selected_product, product)
+                    Assert.equal(feedback_pg.product_filter.selected_version, version)
+                    Assert.equal(feedback_pg.product_from_url, product)
+                    Assert.equal(feedback_pg.version_from_url, version)
+                    Assert.greater(len(feedback_pg.messages), 0)
+                    feedback_pg.product_filter.unselect_version(version)
+                feedback_pg.product_filter.unselect_product(product)

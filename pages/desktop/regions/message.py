4c1befc79f17dd3d54d875b3390a199fce1dae7a
Will Kahn-Greene <willkg@mozilla.com>
2014-05-28 13:09:52 -0400

    Rewrite generic feedback form tests
    This covers:
    
    * submitting happy feedback
    * submitting sad feedback
    * going back and forth through the cards
    * description remaining character count
    * email verification
    * url verification
    
    Fixes #146

--- pages/desktop/regions/message.py
+++ pages/desktop/regions/message.py
@@ -11,33 +11,21 @@ from pages.base import Page
 
 class Message(Page):
 
-    _type_locator = (By.CSS_SELECTOR, '.type')
-    _body_locator = (By.CSS_SELECTOR, '.body')
-    _time_locator = (By.CSS_SELECTOR, 'time')
-    _platform_locator = (By.CSS_SELECTOR, '.meta li:nth-child(2) a')
-    _locale_locator = (By.CSS_SELECTOR, '.meta li:nth-child(3) a')
-    _site_locator = (By.CSS_SELECTOR, '.meta li:nth-child(4)')
-    _more_options_locator = (By.CSS_SELECTOR, '.options')
-    _copy_user_agent_locator = (By.CSS_SELECTOR, '.options .copy_ua')
-    _translate_message_locator = (By.CSS_SELECTOR, '.options ul li:nth-child(2) a')
-    _tweet_this_locator = (By.CSS_SELECTOR, '.options .twitter')
+    _type_locator = (By.CSS_SELECTOR, 'span.sprite')
+    _body_locator = (By.CSS_SELECTOR, 'p')
+    _locale_locator = (By.CSS_SELECTOR, '.meta li:nth-child(4) a')
+    _site_locator = (By.CSS_SELECTOR, '.meta li:nth-child(5)')
+    _permalink_locator = (By.CSS_SELECTOR, '.meta li:nth-child(5) a')
 
     def __init__(self, testsetup, element):
         Page.__init__(self, testsetup)
         self._root_element = element
-
-    def click_platform(self):
-        self._root_element.find_element(*self._platform_locator).click()
 
     def click_locale(self):
         self._root_element.find_element(*self._locale_locator).click()
 
     def click_timestamp(self):
         self._root_element.find_element(*self._time_locator).click()
-
-    @property
-    def is_platform_visible(self):
-        return self.is_element_visible(self._platform_locator)
 
     @property
     def is_locale_visible(self):
@@ -52,17 +40,15 @@ class Message(Page):
         return self._root_element.find_element(*self._body_locator).text
 
     @property
-    def time(self):
-        return self._root_element.find_element(*self._time_locator).text
-
-    @property
-    def platform(self):
-        return self._root_element.find_element(*self._platform_locator).text
-
-    @property
     def locale(self):
         return self._root_element.find_element(*self._locale_locator).text
 
     @property
     def site(self):
         return self._root_element.find_element(*self._site_locator).text
+
+    @property
+    def response_id(self):
+        permalink = self._root_element.find_element(*self._permalink_locator)
+        link = permalink.get_attribute('href')
+        return link.split('/')[-1]
